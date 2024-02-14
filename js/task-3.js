1e704827d2766bb70e19cee575f6adfc69e51da5
Trtl <47746034+Olegka-V@users.noreply.github.com>
2024-02-14 22:26:57 +0200

    done 3 task

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,22 @@
-function getElementWidth(content, padding, border) {
-    // 1. Всі елементи цифрами
-    const parseContent = Number.parseFloat(content);
-    const parsePadding = Number.parseFloat(padding);
-    const parseBorder =  Number.parseFloat(border);
-    // 2. Перемноження аргументів
-    const result = parseContent + parsePadding * 2 + parseBorder * 2;
-    // 3. Повернути значення
-    return result;
-
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    getInfo() {
+        return ` ${this.username} has ${this.playTime} active hours!`; 
+    },
+    changeUsername(newName) { 
+        this.username = newName ;
+    },
+    updatePlayTime(hours) { 
+        this.playTime += hours ;
+    },
     
-}
+};
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
